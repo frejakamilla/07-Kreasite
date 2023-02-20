b45cd9cb8a7af0aa71e71a4966b75aa0f3668d9e
Anahita Sarafraz <anah0116@stud.kea.dk>
2023-02-20 11:14:50 +0100

    Js fetch

---
+++ JS/fetch.js
@@ -0,0 +1,25 @@
+// Variabler
+const url = "https://kreahobby-0fd9.restdb.io/rest/kreahobby";
+
+// Det hemmelige kodeord
+const options = {
+  headers: {
+    "x-apikey": "63ecb0af478852088da682ed",
+  },
+};
+
+// fetch - Hente data
+async function hentData() {
+  const resultat = await fetch(url, options);
+  const json = await resultat.json();
+  visData(json);
+}
+
+hentData();
+
+// visdata
+function visData(jsonFilData) {
+  jsonFilData.forEach((hobby) => {
+    console.log(hobby);
+  });
+}
