e62b0d2c151427743a3b5ee021ce1c2dc2697c03
freja <kami7344@stud.kea.dk>
2023-02-22 13:11:54 +0100

    kategori og hobbylist koblet med JS

--- JS/hobby-list.js
+++ JS/hobby-list.js
@@ -1,55 +1,38 @@
-// Variabler
-const url = "https://kreahobby-0fd9.restdb.io/rest/kreahobby";
+console.log("vi er i hobby-list.html");
 
-// Det hemmelige kodeord
+const urlParams = new URLSearchParams(window.location.search);
+const kat = urlParams.get("kat");
+const url = `https://kreahobby-0fd9.restdb.io/rest/kreahobby?kategori=${kat}`;
+
+// vælg overskrifter og brødkrummesti
+
 const options = {
   headers: {
     "x-apikey": "63ecb0af478852088da682ed",
   },
 };
 
-// fetch - Hente data
 async function hentData() {
-  const resultat = await fetch(url, options);
-  const json = await resultat.json();
-  visData(json);
+  const response = await fetch(url, options);
+  const data = await response.json();
 
-  json.forEach(visHobby);
+  data.forEach(visHobby);
 }
 
 hentData();
 
-// visdata
-function visData(jsonFilData) {
-  jsonFilData.forEach((hobby) => {
-    // console.log(hobby);
-    if (hobby.kategori.includes("Stof")) {
-      console.log(hobby);
-    }
-  });
-}
-// -------------------------------------------------------------------------------------
-
-console.log("produktliste.js");
-//https://kea-alt-del.dk/t7/api/products
-
-const urlParams = new URLSearchParams(window.location.search);
-const kat = urlParams.get("kat");
-
-document.querySelector(".breadcrumb hobby-kategori").textContent = kat;
-document.querySelector(".hobby-card h4").textContent = kat;
+document.querySelector(".breadcrumb .aktiv-kategori").textContent = hobby.kategori;
 
 function visHobby(hobby) {
   console.log(hobby);
-  //4. fange vores template
   const template = document.querySelector("#hobby-card-template").content;
-  //5. clone
   const klon = template.cloneNode(true);
   //6. skifte data
   klon.querySelector("a").href = "hobby.html?id=" + hobby._id;
 
   klon.querySelector("h2").textContent = hobby.navn;
   klon.querySelector("img").src = "assets/" + hobby.billede;
+  klon.querySelector("h4").textContent = hobby.kategori;
 
   //7. appende
   document.querySelector(".card-grid").appendChild(klon);
