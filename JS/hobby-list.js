e241499c59f6f1ba3488589fd0854e5d90d0ccb3
freja <kami7344@stud.kea.dk>
2023-02-23 17:06:35 +0100

    fjerner kommentarer

--- JS/hobby-list.js
+++ JS/hobby-list.js
@@ -3,18 +3,13 @@ console.log("vi er i hobby-list.html");
 const urlParams = new URLSearchParams(window.location.search);
 const kat = urlParams.get("kat");
 const url = `https://kreahobby-0fd9.restdb.io/rest/kreahobby`;
-// const url = `https://omhyggeligt-4c12.restdb.io/rest/ny-database`;
 
-// vælg overskrifter og brødkrummesti
 document.querySelector(".breadcrumb .bread-kategori").textContent = kat;
 
 const options = {
   headers: {
     "x-apikey": "63ecb0af478852088da682ed",
   },
-  // headers: {
-  //   "x-apikey": "63f6152e478852088da68558",
-  // },
 };
 
 async function hentData() {
@@ -32,7 +27,6 @@ function visHobby(hobby) {
     console.log("hobby");
     const template = document.querySelector("#hobby-card-template").content;
     const klon = template.cloneNode(true);
-    //6. skifte data
 
     klon.querySelector("#card-link").href = "hobby.html?id=" + hobby._id + "&kat=" + hobby.kategori;
 
@@ -40,7 +34,6 @@ function visHobby(hobby) {
     klon.querySelector("img").src = "assets/" + hobby.billede;
     klon.querySelector("h4").textContent = hobby.kategori;
 
-    //7. appende
     document.querySelector(".card-grid").appendChild(klon);
   }
 }
--- JS/hobby.js
+++ JS/hobby.js
@@ -2,26 +2,17 @@ const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 const kat = urlParams.get("kat");
 const url = `https://kreahobby-0fd9.restdb.io/rest/kreahobby/${id}`;
-// const url = `https://omhyggeligt-4c12.restdb.io/rest/ny-database/${id}`;
 
 document.querySelector(".breadcrumb .bread-kategori").textContent = kat;
 document.querySelector(".breadcrumb .bread-kategori-link").href = "hobby-list.html?kat=" + kat;
 
-// klon.querySelector("#card-link").href = "hobby.html?id=" + hobby._id + "&kat=" + hobby.kategori;
-
 console.log("visHobbySingleview");
 
-// Det hemmelige kodeord
 const options = {
   headers: {
     "x-apikey": "63ecb0af478852088da682ed",
   },
-  // headers: {
-  //   "x-apikey": "63f6152e478852088da68558",
-  // },
 };
-
-// fetch - Hente data
 async function hentData() {
   const resultat = await fetch(url, options);
   const json = await resultat.json();
@@ -30,7 +21,6 @@ async function hentData() {
 
 hentData();
 
-// visdata
 function vis(hobby) {
   console.log("hobby");
   document.querySelector(".breadcrumb .bread-hobby").textContent = hobby.navn;
